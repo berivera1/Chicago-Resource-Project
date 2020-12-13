7f9575959e8cc7185c06394d2823f0ea54c986fa
Beck R <46095706+BERDesigns@users.noreply.github.com>
2020-12-13 15:18:30 -0600

    SVG Dates...Working?

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,10 @@
 import "./../App.css";
 import "./../style.css";
-import { ReactComponent as WorkIcon } from "./../work.svg";
-import { ReactComponent as SchoolIcon } from "./../school.svg";
+// import { ReactComponent as WorkIcon } from "./../work.svg";
+// import { ReactComponent as SchoolIcon } from "./../school.svg";
 import timelineElements from "../timelineElements";
 import {VerticalTimeline,VerticalTimelineElement, } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
-
 
 function HomePage() {
     let workIconStyles = { background: "#06D6A0" };
@@ -35,7 +34,7 @@ function HomePage() {
                 // date={element.date}
                 dateClassName="date"
                 iconStyle={isWorkIcon ? workIconStyles : schoolIconStyles}
-                icon={isWorkIcon ? <WorkIcon /> : <SchoolIcon />}
+                icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 500 500"><defs><style>.cls-1{'{'}fill:#fff;stroke:#000;stroke-miterlimit:10;{'}'}.cls-2{'{'}font-size:150px;font-family:NimbusSan-Bol, Nimbus Sans;{'}'}</style></defs><g id="CIRCLE"><circle class="cls-1" cx="250" cy="250" r="250"/></g><g id="DATE"><text class="cls-2" transform="translate(84.21 297.7)">{element.startYear}</text></g></svg>}
               >
                 <h3 className="vertical-timeline-element-title">
                   {element.title}
